26ab8726219801c4258fe8d9e49625155041a1ae
andressabgomes10 <andressabgomes10@gmail.com>
2025-07-28 12:44:53 +0000

    auto-commit for e5ca29dc-2800-4fa4-a0fc-6c06e85ba869

--- whatsapp-service/server.js
+++ whatsapp-service/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const axios = require('axios')
 const qrcode = require('qrcode-terminal')
+require('dotenv').config()
 
 const app = express()
 app.use(cors())
